2a006819c2a7e69366164857a0ef360b839b0d38
rsn1 <jonisris@hotmail.com>
2024-12-03 08:36:11 +0100

    day3

--- day3/day3.py
+++ day3/day3.py
@@ -1,9 +1,20 @@
 with open("input.txt") as f:
-    data = f.read()#[list(map(int,x.split())) for x in f.read().splitlines() if x]
+    data = f.read()
 
-print(len(data))
-
+sum = 0
+do = True
 for idx,c in enumerate(data):
+    dowindow = data[idx:idx+4]
+    dontwindow = data[idx:idx+7]
+    
+    if dowindow == "do()":
+        do = True
+    if dontwindow == "don't()":
+        do = False
+    if not do:
+        continue
+
+    #mulwindow
     wdw = data[idx:idx+3]
     if wdw != "mul":
         continue
@@ -15,8 +26,8 @@ for idx,c in enumerate(data):
     #mul(
     num1 = ''
     while(data[current_idx].isdigit()):
+        num1 += data[current_idx]
         current_idx += 1
-        num += data[current_idx]
     #mul(123
     if data[current_idx] != ',':
         continue
@@ -24,9 +35,13 @@ for idx,c in enumerate(data):
     #mul(123,
     num2 = ''
     while(data[current_idx].isdigit()):
+        num2 += data[current_idx]
         current_idx += 1
     #mul(123,54
     if data[current_idx] != ')':
         continue
     
+    sum += int(num1) * int(num2)
+
+print(sum)
 
